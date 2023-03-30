32782b20e4b0d5ec42c7a6f765cf0c2f849a6a47
JeanPiton <jean.orpiton@gmail.com>
2023-03-30 12:59:16 -0300

    bonus

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 let prato, bebida, sobremesa;
 let test = 0;
+const finalizar = document.getElementsByClassName("final");
 function select(element) {
     const collection = document.getElementsByClassName(element.className);
     for (let i = 0; i < collection.length; i++) {
@@ -19,11 +20,24 @@ function select(element) {
     }
     verifica();
 }
-function button() {
+function button(){
+    finalizar[0].style.display="flex";
+    document.getElementById("pprato").innerText = prato[0];
+    document.getElementById("cprato").innerText = prato[1].toFixed(2);
+    document.getElementById("pbebida").innerText = bebida[0];
+    document.getElementById("cbebida").innerText = bebida[1].toFixed(2);
+    document.getElementById("psobremesa").innerText = sobremesa[0];
+    document.getElementById("csobremesa").innerText = sobremesa[1].toFixed(2);
+    document.getElementById("total").innerText = "R$ "+ (prato[1]+bebida[1]+sobremesa[1]).toFixed(2);
+}
+function cancelar(){
+    finalizar[0].style.display = "none";
+}
+function confirmar() {
     const res = (prato[1] + bebida[1] + sobremesa[1]).toFixed(2);
     let pedido = "Olá, gostaria de fazer o pedido:\r\n- Prato: " + prato[0] + "\r\n- Bebida: " + bebida[0] + "\r\n- Sobremesa: " + sobremesa[0] + "\r\nTotal: R$ " + res;
     pedido = encodeURIComponent(pedido);
-    location.href = 'https://wa.me/+5519999869449?text=' + pedido;
+    location.href = 'https://wa.me/?text=' + pedido;
 }
 function verifica() {
     if (prato && bebida && sobremesa) {
