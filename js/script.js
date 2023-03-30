9c38821c920ca0a6f6372f6b10a968909c23370c
JeanPiton <jean.orpiton@gmail.com>
2023-03-30 13:13:15 -0300

    bonus nome e endereço

--- js/script.js
+++ js/script.js
@@ -34,10 +34,12 @@ function cancelar(){
     finalizar[0].style.display = "none";
 }
 function confirmar() {
+    const nome = prompt("Insira o seu nome:");
+    const endereco = prompt("Insira o seu endereço:");
     const res = (prato[1] + bebida[1] + sobremesa[1]).toFixed(2);
-    let pedido = "Olá, gostaria de fazer o pedido:\r\n- Prato: " + prato[0] + "\r\n- Bebida: " + bebida[0] + "\r\n- Sobremesa: " + sobremesa[0] + "\r\nTotal: R$ " + res;
+    let pedido = "Olá, gostaria de fazer o pedido:\r\n- Prato: " + prato[0] + "\r\n- Bebida: " + bebida[0] + "\r\n- Sobremesa: " + sobremesa[0] + "\r\nTotal: R$ " + res + "\r\n\r\nNome: "+nome+"\r\nEndereço: "+endereco;
     pedido = encodeURIComponent(pedido);
-    location.href = 'https://wa.me/?text=' + pedido;
+    location.href = 'https://wa.me/+5519999869449?text=' + pedido;
 }
 function verifica() {
     if (prato && bebida && sobremesa) {
